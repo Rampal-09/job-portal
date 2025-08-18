b49a6f3b496f732943ea77199aa82b8ea25f6368
Rampal-09 <rampaltanwar0929@gmail.com>
2025-08-18 23:51:48 +0530

    Setup session, create .env and .gitignore, backend ready

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session")(session);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 5000;
-const Mongo_URI =
-  "mongodb+srv://rampaltanwar0929:Rst%2309%3F29@clustertest.3gcxkjq.mongodb.net/jobPortalDB";
+const store = new mongodbStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+  })
+);
 
 mongoose
-  .connect(Mongo_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on  http://localhost port ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server running on  http://localhost port ${process.env.PORT}`
+      );
     });
   })
   .catch((err) => console.error("MongoDB connection error:", err));
