e4cfc0afdb25278cd696e6be790598baeae38bdd
Rampal-09 <rampaltanwar0929@gmail.com>
2025-08-19 14:52:02 +0530

    Implement login, and logout functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,23 +2,63 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 exports.postSignUp = async (req, res, next) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-  if (existingUser) {
-    return res.status(400).json({ message: "email is already exist" });
+    if (existingUser) {
+      return res.status(400).json({ message: "email is already exist" });
+    }
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "user created succesfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
   }
-  const hashPassword = await bcrypt.hash(password, 12);
-
-  const newUser = new User({
-    name,
-    email,
-    password: hashPassword,
-  });
-
-  await newUser.save();
-  res.status(201).json({ message: "user created succesfully" });
 };
 
-exports.postLogin = (req, res, next) => {};
+exports.postLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    req.session.user = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+
+    req.session.isLoggedIn = true;
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((error) => {
+    if (error) {
+      return res.status(500).json({ message: "Logout failed" });
+    }
+    res.status(200).json({ message: "logout successfuly" });
+  });
+};
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,11 +1,9 @@
-const express =  require('express');
+const express = require("express");
 const authRouter = express.Router();
-const authController =  require("../controllers/authController")
+const authController = require("../controllers/authController");
 
-
-authRouter.post("/signUp" ,  authController.postSignUp )
-
-
-
+authRouter.post("/signUp", authController.postSignUp);
+authRouter.post("/login", authController.postLogin);
+authRouter.post("/logout", authController.postLogout);
 
 exports.authRouter = authRouter;
