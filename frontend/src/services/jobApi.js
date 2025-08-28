28b3c3d8cefa966ed691dfa0c1a11ba7efe887e1
Rampal-09 <rampaltanwar0929@gmail.com>
2025-08-28 22:49:20 +0530

    fix(api): update getJobs to handle async/await correctly

--- frontend/src/services/jobApi.js
+++ frontend/src/services/jobApi.js
@@ -2,11 +2,34 @@ import axios from "axios";
 const API_URL = "http://localhost:5000";
 
 export const createJob = async (jobData) => {
-  console.log("data sent to jobRouter");
   try {
     console.log("res go to from jobRouter");
     const res = await axios.post(`${API_URL}/jobs`, jobData);
-    console.log("res come from jobController", res);
+
+    return res.data;
+  } catch (err) {
+    if (err.response?.status === 401) {
+      throw {
+        type: "auth",
+        message: "please login first",
+      };
+    } else if (err.response?.status === 400) {
+      throw {
+        type: "client",
+        message: err.response?.data?.message,
+      };
+    } else {
+      throw {
+        message: err.response?.data.message,
+      };
+    }
+  }
+};
+
+export const getJobs = async () => {
+  try {
+    const res = await axios.get(`${API_URL}/jobs`);
+    console.log("response", res.data);
     return res.data;
   } catch (err) {
     console.log(err);
